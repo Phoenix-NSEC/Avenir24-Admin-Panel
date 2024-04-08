d0efb57d8a91c7ffa1afa64399f4551c56857811
Abhinil-07 <abhinilnath10@gmail.com>
2024-04-08 15:17:45 +0530

    Small UI change

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center pt-8  h-screen">
+    <div className="flex flex-col gap-4 items-center justify-center pt-8  h-screen">
       Home page
+      <Link href={"/events"}>
+        <Button>Events</Button>
+      </Link>
     </div>
   );
 }
