deaf698d0f5173c119efc47de2849020d9f08341
unknown <abhinilnath10@gmail.com>
2024-04-26 18:43:18 +0530

    Added event registration routes

--- components/testForm.tsx
+++ components/testForm.tsx
@@ -14,47 +14,126 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+import { TeventDetailsSchema, eventDetailsSchema } from "@/lib/types";
 
 export function ProfileForm() {
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<TeventDetailsSchema>({
+    resolver: zodResolver(eventDetailsSchema),
     defaultValues: {
-      username: "",
+      name: "",
+      description: "",
+      registrationFees: "", 
+      subCategory: "",
+
     },
   });
-
+  const fileRef = form.register("rulebook");
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  function onSubmit(data: TeventDetailsSchema) {
+    const formData = new FormData();
+    const requestData = {
+      name: data.name,
+      description: data.description,
+      registrationFees: data.registrationFees,
+      subCategory: data.subCategory,
+    };
+    console.log(requestData);
+    formData.append("rulebook", data.rulebook[0]);
+    formData.append("data", JSON.stringify(requestData));
+    console.log(formData)
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full p-10 space-y-6"
+      >
         <FormField
           control={form.control}
-          name="username"
+          name="name"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel className="text-lg text-white">Event Name</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="Eg: Hackerthon" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
+              <FormDescription className="text-white">
+                Enter the name of the event
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+         <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg text-white">Description</FormLabel>
+              <FormControl>
+                <Input placeholder="Description" {...field} />
+              </FormControl>
+              <FormDescription className="text-white">
+                Enter the description for the event
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="subCategory"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg text-white">Sub Category</FormLabel>
+              <FormControl>
+                <Input placeholder="Eg: Cybernix" {...field} />
+              </FormControl>
+              <FormDescription className="text-white">
+                Enter the wing under which the event is
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="registrationFees"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg text-white">
+                Registration Fees
+              </FormLabel>
+              <FormControl>
+                <Input placeholder="Eg:50" {...field} />
+              </FormControl>
+              <FormDescription className="text-white">
+                Enter the registration fees for the event
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="rulebook"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>File</FormLabel>
+                <FormControl>
+                  <Input type="file" placeholder="shadcn" {...fileRef} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <Button disabled={form.formState.isSubmitting} type="submit">
+          {form.formState.isSubmitting ? "Submitting" : "Submit"}
+        </Button>
       </form>
     </Form>
   );
--- lib/types.ts
+++ lib/types.ts
@@ -17,24 +17,24 @@ export const eventDetailsSchema = z.object({
     message: "Subcategory cannot be empty",
   }),
 
-  coordinators: z
-    .string()
-    .regex(/^(\w+\s\w+\s:\s\d{10})(,\s\w+\s\w+\s:\s\d{10})*$/),
+  // coordinators: z
+  //   .string()
+  //   .regex(/^(\w+\s\w+\s:\s\d{10})(,\s\w+\s\w+\s:\s\d{10})*$/),
 
-  particpants: z.string().min(1, {
-    message: "Participants cannot be empty",
-  }),
+  // particpants: z.string().min(1, {
+  //   message: "Participants cannot be empty",
+  // }),
 
-  prizeMoney: z.string().min(1, {
-    message: "Prize money cannot be empty",
-  }),
+  // prizeMoney: z.string().min(1, {
+  //   message: "Prize money cannot be empty",
+  // }),
 
-  date: z.string().min(1, {
-    message: "Date cannot be empty",
-  }),
+  // date: z.string().min(1, {
+  //   message: "Date cannot be empty",
+  // }),
   rulebook: z.any(),
 
-  imgUrl: z.any(),
+  // imgUrl: z.any(),
 });
 
 export type TeventDetailsSchema = z.infer<typeof eventDetailsSchema>;
