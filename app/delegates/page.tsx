63babdf92759d4704b980861ae77413624a19fb0
Abhinil-07 <abhinilnath10@gmail.com>
2024-05-05 15:27:46 +0530

    Added UI for delegates'details

---
+++ app/delegates/page.tsx
@@ -0,0 +1,12 @@
+import { Delegates } from "@/components/component/delegates";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Delegates />
+    </div>
+  );
+};
+
+export default page;
